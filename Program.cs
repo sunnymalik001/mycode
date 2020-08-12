ffbdaeb7c0470deff403e338966f45f006c951cc
sunnymalik001@gmail.com <sunnymalik001@gmail.com>
2020-08-12 16:50:12 +0530

    dev 2 branch chnages

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! sunny this is git hub");
+            global::System.Console.WriteLine("this is dev 2 branch");
         }
     }
 }
+
